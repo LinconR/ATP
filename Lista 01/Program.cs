c6521c2aba6243f4b910123e094fc7428dfcb1f3
LinconR <lincon.schaper92@gmail.com>
2024-04-03 22:13:59 -0300

    meu primeiro commit

---
+++ Lista 01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Entrada*/
+            Console.WriteLine( "Digite a Base do Retangulo:");
+            double base1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a Altura do Retangulo:");
+            double altura = double.Parse(Console.ReadLine());
+
+            /*Processamento*/
+            double perimetro = ((base1 * 2) + (altura * 2));
+            double area = (base1 * altura);
+            double dig = Math.Sqrt(Math.Pow(base1,2)+ Math.Pow(altura, 2));
+
+            /*Saida*/
+
+            Console.WriteLine(" Area: " + area);
+            Console.WriteLine(" Perimetro: " + perimetro);
+            Console.WriteLine(" Diagonal: " + dig);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}

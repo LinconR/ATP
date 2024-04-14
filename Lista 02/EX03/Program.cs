42abcd3f203c6d7117fc8225ba2e4ed48186ec6a
Lincon Rangel <111812733+LinconR@users.noreply.github.com>
2024-04-14 13:13:18 -0300

    Add files via upload

---
+++ Lista 02/EX03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Faça um programa para ler os coeficientes de uma equação do primeiro grau (ax + b = 0),
+calcular e escrever a raiz da equação.*/
+
+            /*Entrada de dados */
+
+            Console.WriteLine("Digite os coeficientes da equação:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int 
+
+            }
+    }
+}
